d9fb9b9deec19b2aea9eafc45f6c98b7637c20bb
Adithyan A <147944359+Adithyan0A@users.noreply.github.com>
2024-10-15 21:14:04 +0530

    Add files via upload

---
+++ InfiniteNumberPrinter.java
@@ -0,0 +1,9 @@
+public class InfiniteNumberPrinter {
+    public static void main(String[] args) {
+        int i = 0;
+        while (true) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
